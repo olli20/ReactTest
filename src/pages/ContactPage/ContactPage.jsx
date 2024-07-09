385bc4e4aab1e3ae663fef526573988539f70dfb
Olena Voina <105532756+olli20@users.noreply.github.com>
2024-07-09 16:35:29 +0200

    try to make a netlify form

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
 import PageContainer from "../../common/components/PageContainer/PageContainer"
 
 import { IoMail, IoLogoGithub, IoLogoLinkedin   } from "react-icons/io5";
 
 import css from "./ContactsPage.module.css";
+import PostContainer from "../../common/components/PostContainer/PostContainer";
+
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
 
 const ContactPage = () => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: ""
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({
+                "form-name": form.getAttribute("name"),
+                ...formData
+            })
+        })
+            .then(() => alert("Success!"))
+            .catch((error) => alert(error));
+    };
+
+
     return (
         <PageContainer title="Contacts" description="Contacts">
             <ul className={css.container}>
@@ -21,6 +56,34 @@ const ContactPage = () => {
                     <IoLogoGithub className={css.icon}/>GitHub</a>
                 </li>
             </ul>
+            <PostContainer>
+                <form 
+                    name="contact" 
+                    method="POST" 
+                    data-netlify="true" 
+                    onSubmit={handleSubmit}
+                >
+                    <input type="hidden" name="form-name" value="contact" />
+                    <p>
+                        <label>
+                            Your Name: <input type="text" name="name" value={formData.name} onChange={handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Your Email: <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                            Message: <textarea name="message" value={formData.message} onChange={handleChange} />
+                        </label>
+                    </p>
+                    <p>
+                        <button type="submit">Send</button>
+                    </p>
+                </form>
+            </PostContainer>
         </PageContainer>
     );
 }

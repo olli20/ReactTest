6684830b217d5b8f245a2d00fe205cb1f786556d
Olena Voina <105532756+olli20@users.noreply.github.com>
2024-07-09 23:55:16 +0200

    in progress

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
                 <p>Focused on mastering new skills and best practices, I aim to deliver innovative and efficient 
                 web solutions. Here, you can explore my latest projects in the <Link to={"/projects"} className={css.link}>Projects section</Link> and keep 
                 up with my insights and updates in the <Link to={"/blog"} className={css.link}>Blog section</Link>.</p>
-                <p>If you have questions or collaboration proposals, please <Link to={"/contact"} className={css.link}>contact me</Link>.</p>
+                <p>If you have questions or collaboration proposals, please feel free to <Link to={"/contact"} className={css.link}>contact me</Link>.</p>
             </div>
             </PostContainer>
         </PageContainer>
